8f0b200d79ee32c14e26e35d761ff66968830dd3
oxsoftdev <oxsoftdev@gmail.com>
2017-07-16 09:42:28 -0700

    fix(package)

--- bitsows/__init__.py
+++
@@ -1,7 +0,0 @@
-from .abstracts.pubsub import AbsSubscriber
-
-from .models.OrderUpdate import OrderUpdate
-from .models.StreamUpdate import StreamUpdate
-from .models.TradeUpdate import TradeUpdate
-
-from .client import BitsoClient
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='bitsows'
     , version='0.1'
     , author='sebastian'
     , author_email='oxsoftdev@gmail.com'
-    , packages=['bitsows']
+    , packages=find_packages()
     , url='https://github.com/oxsoftdev/bitsoapi'
     , license='LICENSE.txt'
     , install_requires=['tornado']
