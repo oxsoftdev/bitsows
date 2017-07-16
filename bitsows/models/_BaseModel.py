13208c67df5a6b3d92193bec06d28c3227c87cce
oxsoftdev <oxsoftdev@gmail.com>
2017-07-16 13:27:05 -0400

    fix(typo)

--- bitsows/models/_BaseModel.py
+++ bitsows/models/_BaseModel.py
@@ -10,6 +10,6 @@ class BaseModel:
     def _repr(self, *attrs):
         v = lambda v: str(getattr(self, v, None))
         return ", ".join(
-            map(lambda attr: "{k}={v}".format(k=x, v=v(attr)), attrs)
+            map(lambda attr: "{k}={v}".format(k=attr, v=v(attr)), attrs)
         )
 
