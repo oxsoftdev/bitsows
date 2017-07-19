a8eef3d5a5eef699bf65f8e59a5655742d74ec8c
oxsoftdev <oxsoftdev@gmail.com>
2017-07-19 18:28:04 -0400

    update(order)

--- bitsows/models/TradeUpdate.py
+++ bitsows/models/TradeUpdate.py
@@ -13,17 +13,17 @@ class TradeUpdate(BaseModel):
         for (param, value) in kwargs.items():
             if param == 'i':
                 setattr(self, 'tid', int(value))
-            elif param == 'r':
-                setattr(self, 'rate', Decimal(str(value)))
             elif param == 'a':
                 setattr(self, 'amount', Decimal(str(value)))
+            elif param == 'r':
+                setattr(self, 'rate', Decimal(str(value)))
             elif param == 'v':
                 setattr(self, 'value', Decimal(str(value)))
             elif param == 't':
                 if value == 0:
-                    setattr(self, 'type', 'bid')
+                    setattr(self, 'type', 'buy')
                 elif value == 1:
-                    setattr(self, 'type', 'ask')
+                    setattr(self, 'type', 'sell')
             elif param == 'mo' and value:
                 setattr(self, 'maker_order_id', value)
             elif param == 'to' and value:
